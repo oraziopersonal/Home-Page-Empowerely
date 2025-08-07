ec0c38a83d089c57edb32ed2ac1c570086801a3f
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-07 11:01:35 +0200

    Create botton.tsx
    Creazione dei quattro pulsanti della sidebar

---
+++ botton.tsx
@@ -0,0 +1,10 @@
+function Button {
+  return (
+    <>
+      <button id="user">Crea Utente</button>
+      <button class="option">Crea Survery</button>
+      <button class="option">Richieste Ticket</button>
+      <button id="alert">Criticità</button>
+    </>
+    )
+}
