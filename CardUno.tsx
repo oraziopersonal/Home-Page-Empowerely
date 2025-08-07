f9545ac37f038d16bfa5c049f03967afdf301fe8
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-07 15:42:37 +0200

    Create CardUno.tsx
    Ho creato la prima delle quattro card

---
+++ CardUno.tsx
@@ -0,0 +1,9 @@
+class CardOne() {
+  <>
+    <h3>MAJOR UPDATE - Sezione Utenti Critici</h3>
+    <p>Monitoraggio del bournout > 3 disponibile per gli HR.</p>
+    <button>Generale</button>
+    <button>Nuovo</button>
+    <text>03/08/2025</text>
+  </>
+}
