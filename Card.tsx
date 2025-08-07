fc2b27c3bdd510187aae90e795be9d381863ea84
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-07 13:01:54 +0200

    Create Card.tsx
    Impaginazione della prima sezione filtra aggiornamenti

---
+++ Card.tsx
@@ -0,0 +1,11 @@
+function cardSearch() {
+<>
+<div id="cercaFiltro">
+<form>
+  <label for="cerca">Cerca</label><br>
+    <input type="text" id="campoDiRicerca" name="ricerca"></input>
+  
+</form>
+</div>
+</>
+}
