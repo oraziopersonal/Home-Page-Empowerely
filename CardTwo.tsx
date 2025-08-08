befac322ba4cab7d4b82365951097d5b1660a816
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-08 11:54:56 +0200

    Update CardTwo.tsx

--- CardTwo.tsx
+++ CardTwo.tsx
@@ -5,7 +5,7 @@ function cardTwo() {
         <h3>Nuova selezione Feedback disponibile (solo per HR)</h3>
         <p>E' ora possibile inviare feedback diretti ad Empowerely</p>
         <button id="btn-card-2">Generale</button>
-        <button id="btn-card-new>Nuovo</button>
+        <button id="btn-card-new">Nuovo</button>
         <text>03/08/2025</text>
       </div>
     </>
