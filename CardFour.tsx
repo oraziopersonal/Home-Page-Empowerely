5dc3fdda4a0b3b40740f3cece59359949543308f
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-08 14:41:19 +0200

    Create CardFour.tsx
    creazione della quarta e ultima card

---
+++ CardFour.tsx
@@ -0,0 +1,13 @@
+function cardFour () {
+render (
+  <>
+  <div id="card">Miglioramenti>
+    <h3>Gestione utente migliorata</h3>
+    <p>Nuove funzionalità di amministrazione e importazione nuovi utenti da CSV</p>
+    <button id="btn-carica">Velocità caricamento</button>
+    <button id="btn-relase">Rilascio</button>
+    <text>31/07/2025</text>
+  </div>
+  </>
+)
+}
