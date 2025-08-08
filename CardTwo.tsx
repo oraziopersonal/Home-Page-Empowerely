3abec3378d0949d872fa5428383cfb86af9feca4
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-08 11:50:49 +0200

    Create CardTwo.tsx
    Creating a second card

---
+++ CardTwo.tsx
@@ -0,0 +1,13 @@
+function cardTwo() {
+  render (
+    <>
+      <div>
+        <h3>Nuova selezione Feedback disponibile (solo per HR)</h3>
+        <p>E' ora possibile inviare feedback diretti ad Empowerely</p>
+        <button id="btn-card-2">Generale</button>
+        <button id="btn-card-new>Nuovo</button>
+        <text>03/08/2025</text>
+      </div>
+    </>
+    )
+}
