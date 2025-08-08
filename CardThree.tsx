29bbcf2171970e7812c1d5b9452c8595a5dd16b9
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-08 12:56:21 +0200

    Create CardThree.tsx
    create card three markup

---
+++ CardThree.tsx
@@ -0,0 +1,11 @@
+function CardThree() {
+  <>
+    <div>
+    <h3 id="title">Miglioramento Performqace API</h3>
+    <p>Ottimizzazione delle chiamate API per risposte più rapide</p>
+    <button id="btn-uno">Velocità caricamento</button>
+    <button id="btn-due">Miglioramento</button>
+    <text>31/07/2025</text>
+    </div>
+  </>
+};
