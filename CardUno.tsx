bef5015cdb14eb73626238cad20a451063adee0a
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-08 11:25:29 +0200

    Update CardUno.tsx
    Add clas card to general div and id to button

--- CardUno.tsx
+++ CardUno.tsx
@@ -1,9 +1,11 @@
 class CardOne() {
   <>
+    <div class="Card">
     <h3>MAJOR UPDATE - Sezione Utenti Critici</h3>
     <p>Monitoraggio del bournout > 3 disponibile per gli HR.</p>
-    <button>Generale</button>
-    <button>Nuovo</button>
+    <button id="general">Generale</button>
+    <button id="new">Nuovo</button>
     <text>03/08/2025</text>
+    </div>
   </>
 }
